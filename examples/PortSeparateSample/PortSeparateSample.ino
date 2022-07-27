a6826ba75e3a34c9406464d45d139263005292f8
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2022-07-27 22:26:44 +0900

    add PortSeparateSample

---
+++ examples/PortSeparateSample/PortSeparateSample.ino
@@ -0,0 +1,2 @@
+/// This is an empty sketch for ArduinoIDE and PlatformIO compatibility.
+/// The source code is located in the src folder.
---
+++ examples/PortSeparateSample/src/src.cpp
@@ -0,0 +1,111 @@
+
+
+/// M5ライブラリは M5Stack.hやM5Core2.hを使用しても良いが、
+/// この例では 旧CoreとCore2のどちらにも対応可能なM5Unifiedを使用する。
+
+#include <M5Unified.hpp>  // https://github.com/M5Stack/M5Unified/
+#include <esp_dmx.h>      // https://github.com/someweisguy/esp_dmx/
+
+/// ESP_DMXライブラリはメジャーバージョンアップ時に色々な名称変更があったため、
+/// どちらのバージョンにも対応できるようにdefine値に基づいたチェックを行う。
+#if defined(DMX_PACKET_TIMEOUT_TICK)
+#define ESP_DMX_VERSION 2
+#else
+#define ESP_DMX_VERSION 1
+#endif
+
+// パススルー構成の時は送受信をひとつのシリアルポートで行うが、
+// セパレート構成の時は送信と受信を別々のシリアルポートで行う。
+
+/// 送信に使用するUARTのポート番号
+static constexpr const dmx_port_t dmxTxPort = 1;
+
+/// 受信に使用するUARTのポート番号
+static constexpr const dmx_port_t dmxRxPort = 2;
+
+static constexpr const int dmxInterruptPriority = 1;
+static constexpr const int dmxQueueSize         = 1;
+
+QueueHandle_t dmxTxQueue;
+QueueHandle_t dmxRxQueue;
+
+/// 受信データ
+uint8_t in_data[DMX_MAX_PACKET_SIZE];
+
+/// 送信データ
+uint8_t out_data[DMX_MAX_PACKET_SIZE];
+
+void setup(void) {
+    M5.begin();
+
+    /// Core2用のピン設定を用意。
+    /// For M5Stack Core2/Tough pin setting: TX:19  RX:35  EN:27
+    gpio_num_t transmitPin = GPIO_NUM_19;
+    gpio_num_t receivePin  = GPIO_NUM_35;
+    gpio_num_t enablePin   = GPIO_NUM_27;
+
+    /// M5Unifiedの実行環境チェックを利用し、旧Coreでの動作の場合はピン設定を変更する。
+    if (M5.getBoard() == m5::board_t::board_M5Stack) {
+        /// M5Stack(BASIC/GRAY/GO/FIRE) pin setting: TX:13  RX:35  EN:12
+        transmitPin = GPIO_NUM_13;
+        receivePin  = GPIO_NUM_35;
+        enablePin   = GPIO_NUM_12;
+    }
+
+    /// 送信用と受信用の両方のシリアルポートを準備する。
+    dmx_config_t dmxConfig = DMX_DEFAULT_CONFIG;
+    dmx_param_config(dmxTxPort, &dmxConfig);
+    dmx_param_config(dmxRxPort, &dmxConfig);
+
+    /// 送信側ポートは Tx と En のみ使用する。Rx は使用しない。
+    dmx_set_pin(dmxTxPort, transmitPin, -1, enablePin);
+
+    /// 受信側ポートは Rx のみを使用する。Tx と En は使用しない。
+    dmx_set_pin(dmxRxPort, -1, receivePin, -1);
+
+    /// esp_dmxドライバを準備する。
+    dmx_driver_install(dmxTxPort, DMX_MAX_PACKET_SIZE, dmxQueueSize,
+                       &dmxTxQueue, dmxInterruptPriority);
+    dmx_driver_install(dmxRxPort, DMX_MAX_PACKET_SIZE, dmxQueueSize,
+                       &dmxRxQueue, dmxInterruptPriority);
+
+#if ESP_DMX_VERSION == 1
+    dmx_set_mode(dmxRxPort, DMX_MODE_RX);
+    dmx_set_mode(dmxTxPort, DMX_MODE_TX);
+#else
+    dmx_set_mode(dmxRxPort, DMX_MODE_READ);
+    dmx_set_mode(dmxTxPort, DMX_MODE_WRITE);
+#endif
+}
+
+void loop(void) {
+
+    dmx_event_t packet;
+
+    /// RX側からDMXデータを受信する。
+    if (xQueueReceive(dmxRxQueue, &packet, DMX_RX_PACKET_TOUT_TICK)) {
+        if (packet.status == DMX_OK) {
+            if (ESP_OK ==
+                dmx_read_packet(dmxRxPort, in_data, packet.size)) {
+
+// このサンプルでは memcpyを使って、入力内容をそのまま出力内容にコピーする。
+                memcpy(out_data, in_data, DMX_MAX_PACKET_SIZE);
+// ※ ここで値を変更したり、MIDI等から信号をミックスすることができる。
+
+            }
+        }
+    }
+
+    // TX側からDMXデータを送信する。
+    dmx_write_packet(dmxTxPort, out_data, DMX_MAX_PACKET_SIZE);
+
+#if ESP_DMX_VERSION == 1
+    if (ESP_ERR_TIMEOUT != dmx_wait_tx_done(dmxTxPort, DMX_TX_PACKET_TOUT_TICK)) {
+        dmx_tx_packet(dmxTxPort);
+    }
+#else
+    if (ESP_ERR_TIMEOUT != dmx_wait_send_done(dmxTxPort, DMX_TX_PACKET_TOUT_TICK)) {
+        dmx_send_packet(dmxTxPort, DMX_MAX_PACKET_SIZE);
+    }
+#endif
+}
