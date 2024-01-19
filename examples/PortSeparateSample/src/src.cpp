844b745f5ba2a77f3be7bfaa898028106da5c43c
kokichi <ahoaho0992@yahoo.co.jp>
2024-01-18 22:03:10 -0800

    Update src.cpp
    convert tab to space

--- examples/PortSeparateSample/src/src.cpp
+++ examples/PortSeparateSample/src/src.cpp
@@ -9,11 +9,11 @@
 /// ESP_DMXライブラリはメジャーバージョンアップ時に色々な名称変更があったため、
 /// バージョン1.x, 2.x, 3.1.x に対応できるようにdefine値に基づいたチェックを行う。
 #if defined(ESP_DMX_VERSION_MAJOR)
-	#define ESP_DMX_VERSION 3
+    #define ESP_DMX_VERSION 3
 #elif defined(DMX_PACKET_TIMEOUT_TICK)
-	#define ESP_DMX_VERSION 2
+    #define ESP_DMX_VERSION 2
 #else
-	#define ESP_DMX_VERSION 1
+    #define ESP_DMX_VERSION 1
 #endif
 
 // パススルー構成の時は送受信をひとつのシリアルポートで行うが、
@@ -45,8 +45,8 @@ unsigned long lastUpdate = millis();
 
 void setup(void) {
     M5.begin();
-	/// デバッグ用
-	Serial.begin(115200);
+    /// デバッグ用
+    Serial.begin(115200);
 
     /// Core2用のピン設定を用意。
     /// For M5Stack Core2/Tough pin setting: TX:19  RX:35  EN:27
@@ -55,7 +55,7 @@ void setup(void) {
     gpio_num_t enablePin   = GPIO_NUM_27;
 
     /// M5Unifiedの実行環境チェックを利用し、CoreS3,旧Coreでの動作の場合はピン設定を変更する。
-	if (M5.getBoard() == m5::board_t::board_M5StackCoreS3) {
+    if (M5.getBoard() == m5::board_t::board_M5StackCoreS3) {
         /// M5Stack CoreS3 pin setting: TX:7  RX:10  EN:6
         transmitPin = GPIO_NUM_7;
         receivePin  = GPIO_NUM_10;
@@ -70,7 +70,7 @@ void setup(void) {
 
     /// 送信用と受信用の両方のシリアルポートを準備する。
 #if ESP_DMX_VERSION == 3
-	dmx_config_t dmxConfig = DMX_CONFIG_DEFAULT;
+    dmx_config_t dmxConfig = DMX_CONFIG_DEFAULT;
 #else
     dmx_config_t dmxConfig = DMX_DEFAULT_CONFIG;
     dmx_param_config(dmxTxPort, &dmxConfig);
@@ -85,11 +85,11 @@ void setup(void) {
     /// 受信側ポートは Rx のみを使用する。Tx と En は使用しない。
     dmx_set_pin(dmxRxPort, -1, receivePin, -1);
 #endif
-	
+    
     /// esp_dmxドライバを準備する。
 #if ESP_DMX_VERSION == 3
-	dmx_driver_install(dmxTxPort, &dmxConfig, DMX_INTR_FLAGS_DEFAULT);
-	dmx_driver_install(dmxRxPort, &dmxConfig, DMX_INTR_FLAGS_DEFAULT);
+    dmx_driver_install(dmxTxPort, &dmxConfig, DMX_INTR_FLAGS_DEFAULT);
+    dmx_driver_install(dmxRxPort, &dmxConfig, DMX_INTR_FLAGS_DEFAULT);
 #else
     dmx_driver_install(dmxTxPort, DMX_MAX_PACKET_SIZE, dmxQueueSize,
                        &dmxTxQueue, dmxInterruptPriority);
@@ -104,7 +104,7 @@ void setup(void) {
     dmx_set_mode(dmxRxPort, DMX_MODE_READ);
     dmx_set_mode(dmxTxPort, DMX_MODE_WRITE);
 #elif ESP_DMX_VERSION == 3
-	dmx_set_pin(dmxTxPort, transmitPin, -1, enablePin);
+    dmx_set_pin(dmxTxPort, transmitPin, -1, enablePin);
     dmx_set_pin(dmxRxPort, -1, receivePin, -1);
 #endif
 }
@@ -112,29 +112,29 @@ void setup(void) {
 void loop(void) {
 
 #if ESP_DMX_VERSION == 3
-	dmx_packet_t packet;
+    dmx_packet_t packet;
 #else
     dmx_event_t packet;
 #endif
 
 /// RX側からDMXデータを受信する。
 #if ESP_DMX_VERSION == 3
-	if (dmx_receive(dmxRxPort, &packet, DMX_TIMEOUT_TICK)) {
-		unsigned long now = millis(); //デバッグ用
-		if (!packet.err) {
-			dmx_read(dmxRxPort, in_data, packet.size);
+    if (dmx_receive(dmxRxPort, &packet, DMX_TIMEOUT_TICK)) {
+        unsigned long now = millis(); //デバッグ用
+        if (!packet.err) {
+            dmx_read(dmxRxPort, in_data, packet.size);
 // このサンプルでは memcpyを使って、入力内容をそのまま出力内容にコピーする。
                 memcpy(out_data, in_data, DMX_PACKET_SIZE);
 // ※ ここで値を変更したり、MIDI等から信号をミックスすることができる。
-                	if (now - lastUpdate > 1000) {
-                		Serial.printf("Start code: %d Slot 1: %d Number of Slot: %d\n", in_data[0], in_data[1], packet.size);
-                		lastUpdate = now;
-                	}
-		}
-	}
+                    if (now - lastUpdate > 1000) {
+                        Serial.printf("Start code: %d Slot 1: %d Number of Slot: %d\n", in_data[0], in_data[1], packet.size);
+                        lastUpdate = now;
+                    }
+        }
+    }
 #else
     if (xQueueReceive(dmxRxQueue, &packet, DMX_RX_PACKET_TOUT_TICK)) {
-    	unsigned long now = millis(); //デバッグ用
+        unsigned long now = millis(); //デバッグ用
         if (packet.status == DMX_OK) {
             if (ESP_OK ==
                 dmx_read_packet(dmxRxPort, in_data, packet.size)) {
@@ -142,10 +142,10 @@ void loop(void) {
 // このサンプルでは memcpyを使って、入力内容をそのまま出力内容にコピーする。
                 memcpy(out_data, in_data, DMX_MAX_PACKET_SIZE);
 // ※ ここで値を変更したり、MIDI等から信号をミックスすることができる。
-                	if (now - lastUpdate > 1000) {
-                		Serial.printf("Start code: %d Slot 1: %d Number of Slot: %d\n", in_data[0], in_data[1], packet.size);
-                		lastupdate = now;
-                	}
+                    if (now - lastUpdate > 1000) {
+                        Serial.printf("Start code: %d Slot 1: %d Number of Slot: %d\n", in_data[0], in_data[1], packet.size);
+                        lastupdate = now;
+                    }
             }
         }
     }
@@ -153,7 +153,7 @@ void loop(void) {
 
 // TX側からDMXデータを送信する。
 #if ESP_DMX_VERSION == 3
-	dmx_write(dmxTxPort, out_data, DMX_PACKET_SIZE);
+    dmx_write(dmxTxPort, out_data, DMX_PACKET_SIZE);
 #else
     dmx_write_packet(dmxTxPort, out_data, DMX_MAX_PACKET_SIZE);
 #endif
@@ -167,8 +167,8 @@ void loop(void) {
         dmx_send_packet(dmxTxPort, DMX_MAX_PACKET_SIZE);
     }
 #elif ESP_DMX_VERSION == 3
-	if (ESP_ERR_TIMEOUT != dmx_wait_sent(dmxTxPort, DMX_TIMEOUT_TICK)) {
-		dmx_send(dmxTxPort, DMX_PACKET_SIZE);
-	}
+    if (ESP_ERR_TIMEOUT != dmx_wait_sent(dmxTxPort, DMX_TIMEOUT_TICK)) {
+        dmx_send(dmxTxPort, DMX_PACKET_SIZE);
+    }
 #endif
 }
